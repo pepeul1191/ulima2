7c721bfed79b791cb37439da37497a5e7e739788
Pepe Valdivia <jvaldivia@softweb.pe>
2021-04-16 19:46:15 -0500

    cambiando la conexión a mysql

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('database', 'username', 'password', {
+module.exports = new Sequelize('demo', 'root', '123', {
 	// host: 'localhost',
-	dialect: 'sqlite',
+	dialect: 'mysql',
 	pool: {
 		max: 5,
 		min: 0,
 		idle: 10000
 	},
-	storage: 'db/demo.db',
+	// storage: 'db/demo.db',
 	define: {
+		// nueva liena
 		timestamps: false // true by default
 	}
 });
